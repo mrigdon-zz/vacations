2bb26d754adef3d54efdcade934cf91153da0e34
Matt Rigdon <mrigdon@salesforce.com>
2018-12-25 11:56:43 -0800

    prod fork

--- app/models/application_record.rb
+++ app/models/application_record.rb
@@ -3,7 +3,17 @@ class ApplicationRecord < ActiveRecord::Base
 
   def image_urls(key)
     send(key).map do |image|
-      image.blob.service_url
+      if production?
+        image.blob.service_url
+      else
+        Rails.application.routes.url_helpers.rails_blob_path(image, only_path: true)
+      end
     end
+  end
+
+  private
+
+  def production?
+    @production ||= Rails.env == 'production'
   end
 end
