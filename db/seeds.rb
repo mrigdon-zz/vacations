e1c35cfa290accf4b4cad4d5104fa8841754900a
Matt Rigdon <mrigdon@salesforce.com>
2018-12-25 10:36:25 -0800

    more images

--- db/seeds.rb
+++ db/seeds.rb
@@ -15,6 +15,27 @@ vacations = [
     summary: "Sean Brown's college graduation party",
     images: ['cabo1.jpg', 'cabo2.jpg']
   },
+  {
+    latitude: 64.128288,
+    longitude: -21.827774,
+    title: 'Iceland',
+    year: 2018,
+    summary: "Grant & Matt's stop in Iceland before the Northern Europe Cruise",
+    images: [
+      'il1.JPG',
+      'il2.JPG',
+      'il3.JPG',
+      'il4.JPG',
+      'il5.JPG',
+      'il6.JPG',
+      'il7.JPG',
+      'il8.JPG',
+      'il9.JPG',
+      'il10.JPG',
+      'il11.JPG',
+      'il12.JPG',
+    ]
+  },
 ]
 
 vacations.each do |vacation|
