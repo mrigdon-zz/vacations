6ad6e4877d3c7a6d80e464604bd36b457700a1d7
Matthew Rigdon <rigdonmr@gmail.com>
2019-01-16 20:04:48 -0800

    cleanedup frontend stuff

--- app/javascript/packs/home.js
+++ app/javascript/packs/home.js
@@ -7,20 +7,17 @@
 // To reference this file, add <%= javascript_pack_tag 'home' %> to the appropriate
 // layout file, like app/views/layouts/application.html.erb
 
-import React from 'react';
-import { render } from 'react-dom';
-import App from 'components/App';
-import { createStore, applyMiddleware, compose } from 'redux';
-import thunk from 'redux-thunk';
-import rootReducer from 'reducers';
-import { Provider } from 'react-redux';
-import 'styles/index.scss';
-import 'styles/map.css';
-import 'styles/navbar.css';
-import 'styles/modal.css';
+import React from "react";
+import { render } from "react-dom";
+import App from "components/App";
+import { createStore, applyMiddleware, compose } from "redux";
+import thunk from "redux-thunk";
+import rootReducer from "reducers";
+import { Provider } from "react-redux";
+import "styles/index.scss";
 
-document.addEventListener('DOMContentLoaded', () => {
-  const root = document.getElementById('root');
+document.addEventListener("DOMContentLoaded", () => {
+  const root = document.getElementById("root");
   const vacations = JSON.parse(root.dataset.vacations);
 
   const composeAll = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
