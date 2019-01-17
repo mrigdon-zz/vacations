851208042c9b7c909b276c4dae94adb254ea03a2
Matthew Rigdon <rigdonmr@gmail.com>
2019-01-16 22:11:29 -0800

    rm active storage config

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -29,10 +29,6 @@ Rails.application.configure do
     config.cache_store = :null_store
   end
 
-  # Store uploaded files on the local file system (see config/storage.yml for options)
-  config.active_storage.service = :local
-  config.active_storage.service_urls_expire_in = 99.years
-
   # Print deprecation notices to the Rails logger.
   config.active_support.deprecation = :log
 
