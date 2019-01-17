0563da5c107ed91f14b696e11b36fba2d7a1a1a8
Matthew Rigdon <rigdonmr@gmail.com>
2019-01-16 23:14:40 -0800

    make height full width

--- app/javascript/components/Map.js
+++ app/javascript/components/Map.js
@@ -1,9 +1,9 @@
-import PropTypes from 'prop-types';
-import React, { useState, useEffect } from 'react';
-import drawMap from 'lib/map/drawMap';
-import VacationModal from './VacationModal';
-import cloneDeep from 'lodash/cloneDeep';
-import { connect } from 'react-redux';
+import PropTypes from "prop-types";
+import React, { useState, useEffect } from "react";
+import drawMap from "lib/map/drawMap";
+import VacationModal from "./VacationModal";
+import cloneDeep from "lodash/cloneDeep";
+import { connect } from "react-redux";
 
 function Map({ vacations }) {
   const [openVacationId, setOpenVacationId] = useState(null);
@@ -18,10 +18,12 @@ function Map({ vacations }) {
 
   return (
     <React.Fragment>
-      <div id="chartdiv" />
+      <div className="map__container">
+        <div id="chartdiv" />
+      </div>
       {openVacationId && (
         <VacationModal
-          vacation={vacations.find((v) => v.id === openVacationId)}
+          vacation={vacations.find(v => v.id === openVacationId)}
           onRequestClose={() => setOpenVacationId(null)}
         />
       )}
